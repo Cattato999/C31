5610da3ccfa61f084095788fb0a7cb602160861a
Isa <70447522+Cattato999@users.noreply.github.com>
2020-10-19 18:09:32 -0400

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,11 @@
+class paper{
+    constructor(x,y,radius,options){
+	    var options = {
+            isStatic: false, restitution: 0.3, friction: 0.5, density: 1.2
+        }
+        Bodies.circle
+    }
+    display(){
+        ellipse(x,y,radius,options);
+    }
+}
